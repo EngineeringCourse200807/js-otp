53cd662cc25fa0d1a88feb6d4d94177f4c4c98e8
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-08-17 18:05:26 +0200

    :sparkles: introduce vue test lib and use it for existing test

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,12 +1,13 @@
-import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import {render} from '@testing-library/vue'
 
 describe('HelloWorld.vue', () => {
     it('renders props.msg when passed', () => {
-        const msg = 'new message'
-        const wrapper = shallowMount(HelloWorld, {
-            propsData: {msg}
+        const {getByText} = render(HelloWorld, {
+            props: {msg: 'new message'}
         })
-        expect(wrapper.text()).toMatch(msg)
+
+        getByText('new message')
     })
 });
+
