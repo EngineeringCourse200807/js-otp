87a6430f5dee4fc1fca6ca6be550b3f7aa838193
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-07-29 16:09:51 +0800

    :sparkles: add new dependencies

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Toasted from 'vue-toasted'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
@@ -13,3 +15,5 @@ new Vue({
 }).$mount('#app')
 
 Vue.use(Toasted)
+
+Vue.use(ElementUI)
--- src/views/View.vue
+++ src/views/View.vue
@@ -1,38 +1,30 @@
 <template>
-    <div class="about">
-        <h1>Your random entity is {{result}}</h1>
-        <button class="go" v-on:click="go">Go</button>
-    </div>
+  <div class="about">
+    <h1>Your random entity is {{ result }}</h1>
+    <el-button type="primary" v-on:click="go">Go</el-button>
+  </div>
 </template>
 
 <script>
-    import Api from '@/api'
+import Api from '@/api'
 
-    export default {
-        data: () => ({
-            result: ''
-        }),
-        methods: {
-            go: function () {
-                return Api.randomEntity()
-                    .then(response => {
-                        console.log(response)
-                        this.result = response.data.entries[0].API;
-                        // this.$toasted.show(response.data.entries[0].Link, {
-                        //     type: 'info',
-                        //     position: 'bottom-center',
-                        //     duration: 2000
-                        // })
-                    })
-                    // .catch(err => {
-                    //     console.log(err)
-                    //     this.$toasted.show(err, {
-                    //         type: 'error',
-                    //         position: 'bottom-center',
-                    //         duration: 2000
-                    //     })
-                    // })
-            },
-        }
-    }
+export default {
+  data: () => ({
+    result: ''
+  }),
+  methods: {
+    go: function () {
+      return Api.randomEntity()
+          .then(response => {
+            console.log(response)
+            this.result = response.data.entries[0].API
+            // this.$toasted.show(response.data.entries[0].Link, {
+            //     type: 'info',
+            //     position: 'bottom-center',
+            //     duration: 2000
+            // })
+          })
+    },
+  }
+}
 </script>
