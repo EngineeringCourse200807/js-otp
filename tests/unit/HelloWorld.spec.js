d697e597399949be2c9b1a65567feff20fc0d3f0
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-07-29 15:54:27 +0800

    :sparkles: update the sample test

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,12 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Home from '@/views/Home'
+import {mount} from '@vue/test-utils'
 
 describe('HelloWorld.vue', () => {
     it('renders props.msg when passed', () => {
-        const msg = 'new message'
-        const wrapper = shallowMount(HelloWorld, {
-            propsData: {msg}
-        })
-        expect(wrapper.text()).toMatch(msg)
+        const wrapper = mount(Home)
+
+        expect(wrapper.text()).toMatch('Welcome to Your Vue.js App')
     })
-});
+})
